6ef5aa33726c340514b8c187d627b4233f7d68dc
ustsl <w@imvo.site>
2024-05-25 12:04:18 +0300

    rep

--- dictionaries/serverDictionary.ts
+++ dictionaries/serverDictionary.ts
@@ -1,10 +1,11 @@
 import 'server-only';
-import type { Locale } from '../../i18n.config';
+import { Locale } from '../i18n.config';
+
 
 const dictionaries = {
-  en: () => import('@/dictionaries/en.json').then(module => module.default),
-  tr: () => import('@/dictionaries/tr.json').then(module => module.default),
-  ru: () => import('@/dictionaries/ru.json').then(module => module.default)
+  en: () => import('../dictionaries/en.json').then(module => module.default),
+  tr: () => import('../dictionaries/tr.json').then(module => module.default),
+  ru: () => import('../dictionaries/ru.json').then(module => module.default)
 };
 
 export const getDictionary = async (locale: Locale) => {
